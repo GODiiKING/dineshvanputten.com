851ddf20608fbf9a24dbc486e0dd2a84a9375d2f
GODiiKING <dinozblue@live.com>
2025-09-08 16:18:00 +0200

    cards

--- BattleArena/js/gamemanager.js
+++ BattleArena/js/gamemanager.js
@@ -7,20 +7,44 @@ let GameManager = {
         document.getElementById('text-info-3').style.display = 'none';
         document.getElementById('text-info-4').style.display = 'none';
     },
-
+    // Health, Mana, Strength, Agility, Speed
     resetPlayer: function(classType) {
         switch (classType) {
-            case "Warrior":
+            case "Joker":
                 player = new Player(classType, 200, 0, 200, 100, 50);
                 break;
-            case "Rogue":
-                player = new Player(classType, 100, 0, 100, 150, 200);
+            case "Khalifa":
+                player = new Player(classType, 100, 50, 100, 50, 30);
                 break;
-            case "Mage":
-                player = new Player(classType, 80, 0, 50, 200, 50);
+            case "Gantus":
+                player = new Player(classType, 300, 0, 250, 200, 50);
                 break;
-            case "Hunter":
-                player = new Player(classType, 200, 0, 50, 200, 100);
+            case "Chaos":
+                player = new Player(classType, 150, 0, 50, 200, 200);
+                break;
+            case "Hoodini":
+                player = new Player(classType, 150, 300, 10, 200, 150);
+                break;
+            case "Van Cuba":
+                player = new Player(classType, 130, 150, 50, 100, 100);
+                break;
+            case "Wrazzix":
+                player = new Player(classType, 300, 0, 300, 200, 100);
+                break;
+            case "Mongen":
+                player = new Player(classType, 400, 150, 300, 150, 90);
+                break;
+            case "Opriser":
+                player = new Player(classType, 200, 100, 100, 100, 100);
+                break;
+            case "Ven":
+                player = new Player(classType, 150, 100, 150, 200, 100);
+                break;
+            case "Kami":
+                player = new Player(classType, 150, 150, 150, 200, 100);
+                break;
+            case "Shinblade":
+                player = new Player(classType, 200, 0, 250, 200, 150);
                 break;
         }
         let getInterface = document.querySelector(".interface");
@@ -49,12 +73,31 @@ let GameManager = {
         let getHeader = document.querySelector(".header");
         let getActions = document.querySelector(".actions");
         let getEnemy = document.querySelector(".enemy");
-
-        let enemy00 = new Enemy("Goblin", 100, 0, 50, 100, 100);
-        let enemy01 = new Enemy("Troll", 200, 0, 150, 80, 150);
-        let chooseRandomEnemy = Math.floor(Math.random() * 2);
-        enemy = chooseRandomEnemy === 0 ? enemy00 : enemy01;
-
+    
+        // All possible enemies
+        let enemies = [
+            new Enemy("Joker", 200, 0, 200, 100, 50),
+            new Enemy("Khalifa", 100, 50, 100, 50, 30),
+            new Enemy("Gantus", 300, 0, 250, 200, 50),
+            new Enemy("Chaos", 150, 0, 50, 200, 200),
+            new Enemy("Hoodini", 150, 300, 10, 200, 150),
+            new Enemy("Van Cuba", 130, 150, 50, 100, 100),
+            new Enemy("Wrazzix", 300, 0, 300, 200, 100),
+            new Enemy("Mongen", 400, 150, 300, 150, 90),
+            new Enemy("Opriser", 200, 100, 100, 100, 100),
+            new Enemy("Ven", 150, 100, 150, 200, 100),
+            new Enemy("Kami", 50, 150, 150, 200, 100),
+            new Enemy("Shinblade", 200, 0, 250, 200, 150)
+        ];
+    
+        // Remove the player from possible enemies
+        let possibleEnemies = enemies.filter(e => e.enemyType !== player.classType);
+    
+        // Pick a random enemy
+        let chooseRandomEnemy = Math.floor(Math.random() * possibleEnemies.length);
+        enemy = possibleEnemies[chooseRandomEnemy];
+    
+        // Update interface
         getHeader.innerHTML = "<p>Task: Choose your move!</p>";
         getActions.innerHTML = `<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>`;
         getEnemy.innerHTML = `
@@ -68,6 +111,7 @@ let GameManager = {
                 <p>Speed: ${enemy.speed}</p>
             </div>`;
     }
+    
 };
 
 function Player(classType, health, mana, strength, agility, speed) {
